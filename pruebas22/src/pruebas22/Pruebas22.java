9c689564df6561093cc767b8b3e659437a2654bf
Peter_Sequeira <pitsix23@gmail.com>
2024-05-26 12:14:02 -0600

    subiendo nuevo commit

--- pruebas22/src/pruebas22/Pruebas22.java
+++ pruebas22/src/pruebas22/Pruebas22.java
@@ -14,7 +14,7 @@ public class Pruebas22 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola world xd");
+        System.out.println("Hello world xd");
     }
     
 }
