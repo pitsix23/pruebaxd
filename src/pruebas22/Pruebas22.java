338305021888d942a2d56d148ea9b48b517bf4aa
Peter_Sequeira <pitsix23@gmail.com>
2024-05-26 01:54:30 -0600

    primer commit

---
+++ src/pruebas22/Pruebas22.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebas22;
+
+/**
+ *
+ * @author pitsix23
+ */
+public class Pruebas22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola world");
+    }
+    
+}
